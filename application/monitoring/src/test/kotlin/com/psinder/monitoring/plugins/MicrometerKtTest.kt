67a98faeaf7ca672a476dd70ac1d53166bd7c962
arkadiusz <szastarek@gmail.com>
2022-02-23 18:56:35 +0100

    check without problematic test

--- application/monitoring/src/test/kotlin/com/psinder/monitoring/plugins/MicrometerKtTest.kt
+++ application/monitoring/src/test/kotlin/com/psinder/monitoring/plugins/MicrometerKtTest.kt
@@ -26,16 +26,5 @@ internal class MicrometerKtTest : DescribeSpec({
             }
         }
 
-        it("should install when tracing is enabled") {
-            withTestApplication {
-                mockKtorInstallFunction()
-                mockkObject(TracingConfig)
-                every { TracingConfig.enabled } returns true
-
-                application.configureMicrometer(TracingConfig)
-
-                verify(exactly = 1) { application.install(eq(MicrometerMetrics), any()) }
-            }
-        }
     }
 })
