13d12fc03e85830aecd3f0c2914018f6dd73a17e
arkadiusz <szastarek@gmail.com>
2022-02-25 19:36:09 +0100

    fix koin

--- application/security/src/main/kotlin/com/psinder/auth/AuthModule.kt
+++ application/security/src/main/kotlin/com/psinder/auth/AuthModule.kt
@@ -1,5 +1,6 @@
 package com.psinder.auth
 
+import com.psinder.auth.principal.AuthenticatedAccountProvider
 import com.psinder.auth.principal.AuthorizedAccountAbilityProvider
 import com.psinder.auth.principal.AuthorizedAccountAbilityProviderImpl
 import com.psinder.auth.principal.SecurityContext
@@ -8,5 +9,5 @@ import org.koin.dsl.module
 
 val authModule = module {
     single { AuthorizedAccountAbilityProviderImpl(get()) } bind AuthorizedAccountAbilityProvider::class
-    single { SecurityContext(get()) }
+    single { SecurityContext(get()) } bind AuthenticatedAccountProvider::class
 }
